289300fa041618349c7be597da3c9b4a322f3092
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-17 00:32:07 +0500

    Add SearchFragment with layout and RecyclerView for club recommendations

---
+++ app/src/main/java/com/example/unihub/presentation/home/RecInlineAdapter.kt
@@ -0,0 +1,97 @@
+package com.example.unihub.presentation.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.unihub.R
+import com.example.unihub.data.Clubs
+import com.example.unihub.databinding.ItemRecCardBinding
+import com.example.unihub.databinding.ItemRecInlineBinding
+import com.example.unihub.utils.RcViewItemClickIdCallback
+
+open class RecInlineAdapter: RecyclerView.Adapter<RecInlineAdapter.ClubsViewHolder>() {
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Clubs>() {
+        override fun areItemsTheSame(
+            oldItem: Clubs,
+            newItem: Clubs
+        ): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(
+            oldItem: Clubs,
+            newItem: Clubs
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitList(list: List<Clubs>) {
+        differ.submitList(list)
+    }
+
+    var listenerClickAtItem: RcViewItemClickIdCallback? = null
+    fun setOnFollowClickListener(listener: RcViewItemClickIdCallback) {
+        this.listenerClickAtItem = listener
+    }
+
+    inner class ClubsViewHolder(
+        var binding: ItemRecInlineBinding
+    ) : RecyclerView.ViewHolder(
+        binding.root
+    ) {
+        fun onBind(clubs: Clubs) {
+            binding.run {
+                tvRecClub.text = clubs.name
+                var isFollowed = clubs.isFollowed
+
+//                if (clubs.image.isEmpty())
+//                    ivRecClub.setImageResource(R.drawable.puzzle_club)
+//                else
+//                    Glide.with(itemView.context)
+//                        .load(clubs.image)
+//                        .into(ivRecClub)
+
+                if (isFollowed)
+                    btnFollow.text = "Unfollow"
+                else
+                    btnFollow.text = "Follow"
+
+                btnFollow.setOnClickListener {
+                    if (isFollowed)
+                        btnFollow.text = "Unfollow"
+                    else
+                        btnFollow.text = "Follow"
+                    isFollowed = !isFollowed
+                    listenerClickAtItem?.onClick(clubs.id)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ClubsViewHolder {
+        return ClubsViewHolder(
+            ItemRecInlineBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: ClubsViewHolder,
+        position: Int
+    ) {
+        holder.onBind(differ.currentList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
---
+++ app/src/main/java/com/example/unihub/presentation/home/SearchFragment.kt
@@ -0,0 +1,57 @@
+package com.example.unihub.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.unihub.R
+import com.example.unihub.data.Clubs
+import com.example.unihub.databinding.FragmentSearchBinding
+import com.example.unihub.utils.RcViewItemClickIdCallback
+import com.example.unihub.utils.SpacesItemDecoration
+import com.example.unihub.utils.provideNavigationHost
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        provideNavigationHost()?.hideBottomNavigationBar(false)
+
+        val clubsAdapter = RecInlineAdapter()
+        clubsAdapter.submitList(List(6) { Clubs() })
+
+        clubsAdapter.setOnFollowClickListener(
+            object : RcViewItemClickIdCallback {
+                override fun onClick(id: Int) {
+
+                }
+            }
+        )
+
+        val verticalLinearLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        val horizontalLinearLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        binding.run {
+            btnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+
+            rvClubs.layoutManager = verticalLinearLayoutManager
+            rvClubs.adapter = clubsAdapter
+        }
+    }
+
+}

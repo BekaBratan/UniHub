dce5ab8e4f980b04a95b866355be7ec0c27a7dec
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-28 21:46:56 +0500

    Add AddUserFragment and EditUserFragment with corresponding layouts for user management

---
+++ app/src/main/java/com/example/unihub/presentation/admin/AddUserFragment.kt
@@ -0,0 +1,23 @@
+package com.example.unihub.presentation.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.unihub.R
+import com.example.unihub.databinding.FragmentAddUserBinding
+
+class AddUserFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddUserBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAddUserBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}

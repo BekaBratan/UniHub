a69628e281633bab2deb345950803d9d0dc16f71
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-03-28 21:58:25 +0500

    Add RequestsListFragment with layout and RecyclerView for managing requests

---
+++ app/src/main/java/com/example/unihub/presentation/admin/RequestsListFragment.kt
@@ -0,0 +1,23 @@
+package com.example.unihub.presentation.admin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.unihub.R
+import com.example.unihub.databinding.FragmentRequestsListBinding
+
+class RequestsListFragment : Fragment() {
+
+    private lateinit var binding: FragmentRequestsListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRequestsListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}

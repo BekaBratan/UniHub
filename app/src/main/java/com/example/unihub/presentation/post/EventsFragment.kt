65872d9356bdb07ba4643c3b50382a441860a62e
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-04-10 02:52:33 +0500

    Add EventsFragment, update navigation and layout references

---
+++ app/src/main/java/com/example/unihub/presentation/post/EventsFragment.kt
@@ -0,0 +1,23 @@
+package com.example.unihub.presentation.post
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.unihub.R
+import com.example.unihub.databinding.FragmentEventsBinding
+
+class EventsFragment : Fragment() {
+
+    private lateinit var binding: FragmentEventsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEventsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}

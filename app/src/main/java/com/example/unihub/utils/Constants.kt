8d81092dd13bda268ddf871a209f890c4ca051e2
Baibolat Bekarys <210103206@stu.sdu.edu.kz>
2025-05-01 01:24:34 +0500

    Refactor: Update ChatGPT API key in Constants.kt

--- app/src/main/java/com/example/unihub/utils/Constants.kt
+++ app/src/main/java/com/example/unihub/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val BASE_URL = "http://192.168.0.11:5000/api/"
 //        const val BASE_URL = "https://unihub-backend.onrender.com/api/"
-        const val ChatGPTApi = "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"
+        const val ChatGPTApi = "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"
         const val ChatGPT_URL = "https://api.openai.com/"
     }
 }

192cd76ab607dd46867045c7b4a1fd98aa9d4da4
Baibolat Bekarys <92868206+Be1ba@users.noreply.github.com>
2025-05-06 03:52:35 +0500

    Implement ClubViewModel for post creation; update CreateNewPostFragment to handle image selection and error display

--- app/src/main/java/com/example/unihub/presentation/home/posts/PostsViewModel.kt
+++ app/src/main/java/com/example/unihub/presentation/home/posts/PostsViewModel.kt
@@ -21,6 +21,9 @@ class PostsViewModel(): ViewModel() {
     private var _getPostListResponse: MutableLiveData<PostsResponse> = MutableLiveData()
     val getPostListResponse: LiveData<PostsResponse> = _getPostListResponse
 
+    private var _getMyPostsListResponse: MutableLiveData<PostsResponse> = MutableLiveData()
+    val getMyPostsListResponse: LiveData<PostsResponse> = _getMyPostsListResponse
+
     private val _getPostDetailsResponse: MutableLiveData<PostsResponseItem> = MutableLiveData()
     val getPostDetailsResponse: LiveData<PostsResponseItem> = _getPostDetailsResponse
 
@@ -39,6 +42,34 @@ class PostsViewModel(): ViewModel() {
             }.fold(
                 onSuccess = {
                     _getPostListResponse.postValue(it)
+                },
+                onFailure = { throwable ->
+                    val errorMessage = if (throwable is HttpException) {
+                        val errorBody = throwable.response()?.errorBody()?.string()
+                        try {
+                            val json = JSONObject(errorBody ?: "")
+                            json.getString("message")
+                        } catch (e: Exception) {
+                            "Something went wrong."
+                        }
+                    } else {
+                        throwable.message ?: "An unknown error occurred."
+                    }
+                    _errorMessage.postValue(MessageResponse(errorMessage))
+                }
+            )
+        }
+    }
+
+    fun getMyPostsList(token: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                ServiceBuilder.api.getMyPosts(
+                    token = token
+                )
+            }.fold(
+                onSuccess = {
+                    _getMyPostsListResponse.postValue(it)
                 },
                 onFailure = { throwable ->
                     val errorMessage = if (throwable is HttpException) {
--- app/src/main/java/com/example/unihub/presentation/profile/UserProfileFragment.kt
+++ app/src/main/java/com/example/unihub/presentation/profile/UserProfileFragment.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.unihub.R
 import com.example.unihub.databinding.FragmentUserProfileBinding
+import com.example.unihub.presentation.home.posts.HomeFragmentDirections
+import com.example.unihub.presentation.home.posts.PostsAdapter
+import com.example.unihub.presentation.home.posts.PostsViewModel
+import com.example.unihub.presentation.home.posts.RepostBottomSheet
+import com.example.unihub.presentation.home.posts.ShareBottomSheet
+import com.example.unihub.utils.CustomDividerItemDecoration
+import com.example.unihub.utils.RcViewItemClickIdCallback
+import com.example.unihub.utils.SharedProvider
 import com.example.unihub.utils.provideNavigationHost
 
 class UserProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentUserProfileBinding
+    private val postsViewModel: PostsViewModel by viewModels()
+    private val profileViewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +38,56 @@ class UserProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         provideNavigationHost()?.hideBottomNavigationBar(false)
+        val sharedProvider = SharedProvider(requireContext())
+
+        profileViewModel.getUserProfile(sharedProvider.getToken())
+
+        profileViewModel.profileResponse.observe(viewLifecycleOwner) {
+            binding.tvUserName.text = it.name + " " + it.surname
+        }
+
+        postsViewModel.getMyPostsList(sharedProvider.getToken())
+        val postsAdapter = PostsAdapter()
+
+        postsViewModel.getMyPostsListResponse.observe(viewLifecycleOwner) { postList->
+            postsAdapter.submitList(postList)
+        }
+
+        postsAdapter.setOnLikeClickListener(
+            object : RcViewItemClickIdCallback {
+                override fun onClick(id: Int?) {
+                    postsViewModel.likePost(sharedProvider.getToken(), id?: 1)
+                }
+            }
+        )
+
+        postsAdapter.setOnClubNameClickListener(
+            object : RcViewItemClickIdCallback {
+                override fun onClick(id: Int?) {
+                    findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToClubPageFragment(id?: 1, ""))
+                }
+            }
+        )
+
+        postsAdapter.setOnRepostClickListener(
+            object : RcViewItemClickIdCallback {
+                override fun onClick(id: Int?) {
+                    val repostBottomSheet = RepostBottomSheet(id?:1)
+                    repostBottomSheet.show(childFragmentManager, repostBottomSheet.tag)
+                }
+            }
+        )
+
+        postsAdapter.setOnShareClickListener(
+            object : RcViewItemClickIdCallback {
+                override fun onClick(id: Int?) {
+                    val shareBottomSheet = ShareBottomSheet(id?:1)
+                    shareBottomSheet.show(childFragmentManager, shareBottomSheet.tag)
+                }
+            }
+        )
+
+        val verticalLinearLayoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
         binding.run {
             btnSettings.setOnClickListener {
@@ -37,6 +101,17 @@ class UserProfileFragment : Fragment() {
             btnWrite.setOnClickListener {
                 findNavController().navigate(UserProfileFragmentDirections.actionUserProfileFragmentToCreateRequestFragment())
             }
+
+            rvReposts.layoutManager = verticalLinearLayoutManager
+            rvReposts.adapter = postsAdapter
+            rvReposts.addItemDecoration(
+                CustomDividerItemDecoration(
+                    getDrawable(
+                        requireContext(),
+                        R.drawable.divider_1dp
+                    )!!
+                )
+            )
 
             tvReposts.setOnClickListener {
                 rvReposts.visibility = View.VISIBLE
